e7fa4dbdf69bd3458e728017434cb41638d18e1f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-24 21:15:40 +0300

    task67 solved

---
+++ task67/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+int GetSumOfDigitsInNumberRecursive(int N)
+{
+    if (N < 10) return N;
+    else return N % 10 + GetSumOfDigitsInNumberRecursive(N / 10);
+}
+Console.WriteLine(GetSumOfDigitsInNumberRecursive(453));
+Console.WriteLine(GetSumOfDigitsInNumberRecursive(45));
+Console.WriteLine(GetSumOfDigitsInNumberRecursive(121));
