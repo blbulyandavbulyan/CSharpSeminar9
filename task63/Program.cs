bf0e22fe50fadad4d6cf91a49ab3268a88db9cdb
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-24 20:58:48 +0300

    task63 solved

--- task63/Program.cs
+++ task63/Program.cs
@@ -1,9 +1,17 @@
 ﻿//Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
 //N = 5 -> "1, 2, 3, 4, 5"
 //N = 6 -> "1, 2, 3, 4, 5, 6"
-//На данный момент понятия не имею как это сделать
-void RecursiverPrintNaturalNumnbers(int N)
+//как решить без параметра по умолчанию и при этом чтобы вывод был через запятую как в примере представления не имею
+void RecursiverPrintNaturalNumnbers(int N, int notTouchThisValue = 0)
 {
-    Console.Write($"{N}, ");
-    
+    if (notTouchThisValue == 0) notTouchThisValue = N;
+    if (notTouchThisValue == 1)
+    {
+        Console.Write("1, ");
+    }
+    else { 
+        RecursiverPrintNaturalNumnbers(N, notTouchThisValue - 1);
+        Console.Write(N != notTouchThisValue ? $"{notTouchThisValue}, " : $"{notTouchThisValue}");
+    }
 }
+RecursiverPrintNaturalNumnbers(10);
