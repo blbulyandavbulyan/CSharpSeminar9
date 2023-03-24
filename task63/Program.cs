3c5a18729f7739d22dd78b4a317f3610e3f759c2
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-24 20:19:08 +0300

    task64 solved, task64 not solved, but commited

---
+++ task63/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+//На данный момент понятия не имею как это сделать
+void RecursiverPrintNaturalNumnbers(int N)
+{
+    Console.Write(N);
+    if (N == 1)return;
+    else
+    {
+        RecursiverPrintNaturalNumnbers(N);
+
+    }
+}
---
+++ task64/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+void PrintNaturalNumbersFromNToOne(int N)
+{
+    if (N != 1)
+    {
+        Console.Write($"{N}, ");
+        PrintNaturalNumbersFromNToOne(N - 1);
+    }
+    else
+    {
+        Console.WriteLine(1);
+    }
+}
+PrintNaturalNumbersFromNToOne(10);
